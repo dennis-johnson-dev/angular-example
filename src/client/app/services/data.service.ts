63e96829995e0735b3e85a5090a03e89c9a55385
Dennis Johnson <songawee@gmail.com>
2017-12-31 16:33:07 -0700

    baseline

--- src/client/app/services/data.service.ts
+++ src/client/app/services/data.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from "@angular/core";
 
 import { Observable } from "rxjs/Observable";
 import { of } from "rxjs/observable/of";
-import { map } from "rxjs/operators";
+import { map } from "rxjs/operators/map";
 
 export type Datas = {
   foo: boolean;
